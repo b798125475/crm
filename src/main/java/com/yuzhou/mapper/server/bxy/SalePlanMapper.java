88db7a786095ee3157eb470ca907dff170bf321a
sashazhen <59682769+sashazhen@users.noreply.github.com>
2020-01-12 19:19:15 +0800

    Update SalePlanMapper.java

--- src/main/java/com/yuzhou/mapper/server/bxy/SalePlanMapper.java
+++ src/main/java/com/yuzhou/mapper/server/bxy/SalePlanMapper.java
@@ -8,7 +8,6 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
-import com.yuzhou.entity.SaleChance;
 import com.yuzhou.entity.SalePlan;
 
 @Mapper
