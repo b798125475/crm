b88e77c2cd93ac8b713f06e6ffcc3ebf1123f62d
sashazhen <59682769+sashazhen@users.noreply.github.com>
2020-01-12 18:20:18 +0800

    Create SaleChanceServiceImp.java

---
+++ src/main/java/com/yuzhou/service/server/bxy/imp/SaleChanceServiceImp.java
@@ -0,0 +1,51 @@
+package com.yuzhou.service.server.bxy.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yuzhou.entity.SaleChance;
+import com.yuzhou.mapper.server.bxy.SaleChanceMapper;
+import com.yuzhou.service.server.bxy.SaleChanceService;
+
+@Service
+public class SaleChanceServiceImp implements SaleChanceService {
+
+	@Autowired
+	private SaleChanceMapper saleChanceMapper;
+
+	@Override
+	public List<SaleChance> selectSaleChanceAll() {
+		return saleChanceMapper.selectSaleChanceAll();
+	}
+
+	@Override
+	public List<SaleChance> selectSaleChancePaging(SaleChance saleChance) {
+		return saleChanceMapper.selectSaleChancePaging(saleChance);
+	}
+
+	@Override
+	public int insertSaleChance(SaleChance saleChance) {
+		return saleChanceMapper.insertSaleChance(saleChance);
+	}
+
+	@Override
+	public int selectSaleChanceCount(SaleChance saleChance) {
+		// TODO Auto-generated method stub
+		return saleChanceMapper.selectSaleChanceCount(saleChance);
+	}
+
+	@Override
+	public int updateSaleChance(SaleChance saleChance) {
+		// TODO Auto-generated method stub
+		return saleChanceMapper.updateSaleChance(saleChance);
+	}
+
+	@Override
+	public int deleteSaleChance(SaleChance saleChance) {
+		// TODO Auto-generated method stub
+		return saleChanceMapper.deleteSaleChance(saleChance);
+	}
+
+}
