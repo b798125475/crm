322f17eb32bee114fee516afbfda5bc6cab43b0f
b798125475 <59684028+b798125475@users.noreply.github.com>
2020-01-12 19:15:41 +0800

    Create ClientLinkmanController.java

---
+++ src/main/java/com/yuzhou/controller/cust/bxy/ClientLinkmanController.java
@@ -0,0 +1,39 @@
+package com.yuzhou.controller.cust.bxy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yuzhou.entity.ClientLinkman;
+import com.yuzhou.service.cust.bxy.ClientLinkmanService;
+
+@RestController
+public class ClientLinkmanController {
+	@Autowired
+	private ClientLinkmanService clientLinkmanService;
+
+	// 查询客户联系人 通过客户Id查询
+	@RequestMapping("/selectClientLinkmanById")
+	public ClientLinkman selectClientlinkmanById(@RequestBody ClientLinkman clientLinkman) throws Exception {
+		return clientLinkmanService.selectClientlinkmanById(clientLinkman);
+	}
+
+	// 新建客户联系人
+	@RequestMapping("/insertClientLinkman")
+	public int insertClientLinkman(@RequestBody ClientLinkman clientLinkman) throws Exception {
+		return clientLinkmanService.insertClientLinkman(clientLinkman);
+	}
+	
+	//更新客户联系人
+	@RequestMapping("/updateClientLinkman")
+	public int updateClientLinkman(@RequestBody ClientLinkman clientLinkman) throws Exception{
+		return clientLinkmanService.updateClientLinkman(clientLinkman);
+	}
+	
+	//删除联系人
+	@RequestMapping("/deleteClientLinkman")
+	public int deleteClientLinkman(@RequestBody ClientLinkman clientLinkman) throws Exception{
+		return clientLinkmanService.deleteClientLinkman(clientLinkman);	
+	}
+}
