d6a33a3d92fd8ad5718c9287b953da6817b4e3d8
b798125475 <59684028+b798125475@users.noreply.github.com>
2020-01-12 19:17:01 +0800

    Create ClientLinkmanMapper.java

---
+++ src/main/java/com/yuzhou/mapper/cust/bxy/ClientLinkmanMapper.java
@@ -0,0 +1,30 @@
+package com.yuzhou.mapper.cust.bxy;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.yuzhou.entity.ClientLinkman;
+
+@Mapper
+public interface ClientLinkmanMapper {
+	// 通过ID查询客户联系人
+	@Select("SELECT * from clientlinkman where clientCode=#{clientCode} ")
+	public ClientLinkman selectClientlinkmanById(ClientLinkman clientLinkman);
+
+	// 新建客户联系人
+	@Insert("insert into clientLinkman values(null,#{clientLinkmanName},#{clientLinkmanSex},#{clientLinkmanJob},#{clientLinkmanTel},#{clientLinkmanMobile},#{remark})")
+	public int insertClientLinkman(ClientLinkman clientLinkman);
+
+	// 更新客户联系人
+	@Update("update clientLinkman set clientLinkmanName=#{clientLinkmanName},clientLinkmanSex=#{clientLinkmanSex},clientLinkmanJob=#{clientLinkmanJob},clientLinkmanTel=#{clientLinkmanTel},clientLinkmanMobile=#{clientLinkmanMobile},remark=#{remark}  where clientLinkmanId=#{clientLinkmanId}")
+	public int updateClientLinkman(ClientLinkman clientLinkman);
+	
+	//删除客户联系人
+	@Delete("delete from clientLinkman where clientLinkmanId=#{clientLinkmanId}")
+	public int deleteClientLinkman(ClientLinkman clientLinkman);
+	
+}
