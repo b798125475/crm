93c28d8b6b5b4b3dec2bd139ed3ad95ed0b13858
sashazhen <59682769+sashazhen@users.noreply.github.com>
2020-01-15 16:43:54 +0800

    Update SaleChanceService.java

--- src/main/java/com/yuzhou/service/sale/bxy/SaleChanceService.java
+++ src/main/java/com/yuzhou/service/sale/bxy/SaleChanceService.java
@@ -3,6 +3,7 @@ package com.yuzhou.service.sale.bxy;
 import java.util.List;
 
 import com.yuzhou.entity.SaleChance;
+import com.yuzhou.entity.Sysuser;
 
 public interface SaleChanceService {
 
@@ -17,6 +18,14 @@ public interface SaleChanceService {
 	public int updateSaleChance(SaleChance saleChance);
 
 	public int deleteSaleChance(SaleChance saleChance);
-	
+
 	public List<SaleChance> selectSaleChanceByChanceStatus(SaleChance saleChance);
+
+	public SaleChance selectSaleChanceBychanceId(SaleChance saleChance);
+
+	public Sysuser selectSysUserByuserId(Sysuser sysUser);
+	
+	public Sysuser selectSysUserByuserName(Sysuser sysUser);
+	
+	public List<Sysuser> selectSysUserByuserRoleId(Sysuser sysUser);
 }
