5e41812c1a4bd16cc691f11a7de32b2e24af8583
b798125475 <59684028+b798125475@users.noreply.github.com>
2020-01-16 10:33:13 +0800

    Update ClientInfoService.java

--- src/main/java/com/yuzhou/service/cust/bxy/ClientInfoService.java
+++ src/main/java/com/yuzhou/service/cust/bxy/ClientInfoService.java
@@ -3,6 +3,9 @@ package com.yuzhou.service.cust.bxy;
 import java.util.List;
 
 import com.yuzhou.entity.ClientInfo;
+import com.yuzhou.entity.DataArea;
+import com.yuzhou.entity.DataClientLevel;
+import com.yuzhou.entity.Sysuser;
 
 public interface ClientInfoService {
 	// 查询全部客户的方法
@@ -19,4 +22,24 @@ public interface ClientInfoService {
 	
 	//删除客户的方法
 	public int deleteClientById(ClientInfo clientInfo);
+	
+	//查询全部客户经理
+	public List<Sysuser> selectSysuser();
+	
+	//查询全部地区
+	public List<DataArea> selectDataArea();
+
+	//查询全部客户等级
+	public List<DataClientLevel> selectDataClientLevel();
+	
+	//查詢客戶通過ID
+	public ClientInfo selectClientInfoById(ClientInfo clientInfo);
+	
+	
+	//通过ID 查询客户经理
+	public Sysuser selectSysuserById(Sysuser sysUser);
+	
+	//更新客户状态为流失
+	public int updateClientStateById(ClientInfo clientInfo);
+	
 }
