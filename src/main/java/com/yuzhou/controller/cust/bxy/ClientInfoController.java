65f3dd731157e7255c9f1baae447bc33a3c68ffc
b798125475 <59684028+b798125475@users.noreply.github.com>
2020-01-16 10:32:30 +0800

    Update ClientInfoController.java

--- src/main/java/com/yuzhou/controller/cust/bxy/ClientInfoController.java
+++ src/main/java/com/yuzhou/controller/cust/bxy/ClientInfoController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yuzhou.entity.ClientInfo;
+import com.yuzhou.entity.DataArea;
+import com.yuzhou.entity.DataClientLevel;
+import com.yuzhou.entity.Sysuser;
 import com.yuzhou.service.cust.bxy.ClientInfoService;
 
 @RestController
@@ -44,5 +47,43 @@ public class ClientInfoController {
 	public int deleteClientById(@RequestBody ClientInfo clientInfo) throws Exception{
 		return clientInfoService.deleteClientById(clientInfo);
 	}
+	
+	//查询全部客户经理
+	@RequestMapping("/selectSysuser")
+	public List<Sysuser> selectSysuser() throws Exception{
+		return clientInfoService.selectSysuser();
+	}
+	//查询客户经理通过ID
+	@RequestMapping("/selectSysuserById")
+	public Sysuser selectSysuserById(@RequestBody Sysuser sysUser) throws Exception {
+		System.out.println(sysUser.getUserName());
+		return clientInfoService.selectSysuserById(sysUser);
+	}
+	
+	//查询全部地区
+	@RequestMapping("/selectDataArea")
+	public List<DataArea> selectDataArea() throws Exception{
+		return clientInfoService.selectDataArea();
+	}
+	
+	//查询全部客户等级
+	@RequestMapping("/selectDataClientLevel")
+	public List<DataClientLevel> selectDataClientLevel() throws Exception{
+		return clientInfoService.selectDataClientLevel();	
+	}
+	
+	//通過ID查詢客戶
+	@RequestMapping("/selectClientInfoById")
+	public ClientInfo selectClientInfoById(@RequestBody ClientInfo clientInfo) throws Exception{
+		return clientInfoService.selectClientInfoById(clientInfo);
+	}
+
+	//更新客户状态为流失
+	@RequestMapping("/updateClientStateById")
+	public int updateClientStateById(@RequestBody  ClientInfo clientInfo) throws Exception{
+		return clientInfoService.updateClientStateById(clientInfo);
+	}
+	
+	
 
 }
