74d31a3915de0aad6a6bb6f0b490b41629c51f51
b798125475 <59684028+b798125475@users.noreply.github.com>
2020-01-16 10:33:15 +0800

    Update ClientLinkmanService.java

--- src/main/java/com/yuzhou/service/cust/bxy/ClientLinkmanService.java
+++ src/main/java/com/yuzhou/service/cust/bxy/ClientLinkmanService.java
@@ -1,12 +1,14 @@
 package com.yuzhou.service.cust.bxy;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.yuzhou.entity.ClientLinkman;
 
 public interface ClientLinkmanService {
 	// 通过客户编号查询客户联系人
-	public ClientLinkman selectClientlinkmanById(ClientLinkman clientLinkman);
+	public List<ClientLinkman> selectClientlinkmanById(ClientLinkman clientLinkman);
 
 	// 新建客户联系人
 	public int insertClientLinkman(ClientLinkman clientLinkman);
@@ -16,4 +18,7 @@ public interface ClientLinkmanService {
 	
 	//删除客户联系人
 	public int deleteClientLinkman(ClientLinkman clientLinkman);
+	
+	//通过ID查询联系人
+	public ClientLinkman selectclientLinkmanById(ClientLinkman clientLinkman);
 }
