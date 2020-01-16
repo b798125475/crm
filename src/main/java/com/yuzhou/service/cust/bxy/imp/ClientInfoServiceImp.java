8fef5718e1e4aaa9a69cf8584b617b424793a37c
b798125475 <59684028+b798125475@users.noreply.github.com>
2020-01-16 10:33:21 +0800

    Update ClientInfoServiceImp.java

--- src/main/java/com/yuzhou/service/cust/bxy/imp/ClientInfoServiceImp.java
+++ src/main/java/com/yuzhou/service/cust/bxy/imp/ClientInfoServiceImp.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.yuzhou.entity.ClientInfo;
+import com.yuzhou.entity.DataArea;
+import com.yuzhou.entity.DataClientLevel;
+import com.yuzhou.entity.Sysuser;
 import com.yuzhou.mapper.cust.bxy.ClientInfoMapper;
 import com.yuzhou.service.cust.bxy.ClientInfoService;
 
@@ -48,4 +51,44 @@ public class ClientInfoServiceImp implements ClientInfoService {
 		return clientInfoMapper.deleteClientById(clientInfo);
 	}
 
+	//查询全部客户经理
+	@Override
+	public List<Sysuser> selectSysuser() {
+		// TODO Auto-generated method stub
+		return clientInfoMapper.selectSysuser();
+	}
+	
+	//查询全部地区
+	@Override
+	public List<DataArea> selectDataArea() {
+		// TODO Auto-generated method stub
+		return clientInfoMapper.selectDataArea();
+	}
+	//查询全部客户等级
+	@Override
+	public List<DataClientLevel> selectDataClientLevel() {
+		// TODO Auto-generated method stub
+		return clientInfoMapper.selectDataClientLevel();
+	}
+
+	@Override
+	public ClientInfo selectClientInfoById(ClientInfo clientInfo) {
+		// TODO Auto-generated method stub
+		return clientInfoMapper.selectClientInfoById(clientInfo);
+	}
+
+	@Override
+	public Sysuser selectSysuserById(Sysuser sysUser) {
+		// TODO Auto-generated method stub
+		return clientInfoMapper.selectSysuserById(sysUser);
+	}
+
+	//更新客户状态为流失
+	@Override
+	public int updateClientStateById(ClientInfo clientInfo) {
+		// TODO Auto-generated method stub
+		return clientInfoMapper.updateClientStateById(clientInfo);
+	}
+
+	
 }
